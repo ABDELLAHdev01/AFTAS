5c1b46d02090112e9ebd35cbf13fbc1d84120d1c
ABDELLAHdev01 <abdellahelghoulamdev@gmail.com>
2023-12-13 13:51:30 +0100

    adding home dashboard and footer

--- FRONT-END/AFTAS/src/app/compomemnts/competitions/competitions.component.ts
+++ FRONT-END/AFTAS/src/app/compomemnts/competitions/competitions.component.ts
@@ -1,7 +1,16 @@
-import { Component } from '@angular/core';
-import { initFlowbite } from 'flowbite';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  Inject,
+  LOCALE_ID,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { AddModalComponent } from '../add-modal/add-modal.component';
 import { TableComponent } from '../table/table.component';
+import { CompetitionService } from '../../../services/CompetitionService';
 
 
 @Component({
@@ -11,6 +20,15 @@ import { TableComponent } from '../table/table.component';
   templateUrl: './competitions.component.html',
   styleUrl: './competitions.component.css'
 })
-export class CompetitionsComponent {
+export class CompetitionsComponent implements OnInit {
+  comps:any[]= [];
 
+
+
+  ngOnInit(): void {
+    
+  }
+
+ 
+  
 }
