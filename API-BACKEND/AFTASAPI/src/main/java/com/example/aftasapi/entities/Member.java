a3517f7840107036f0522d935368bd1230350f7e
ABDELLAHdev01 <abdellahelghoulamdev@gmail.com>
2024-02-20 09:48:29 +0100

    implementing the UserDetails to Member domain

--- API-BACKEND/AFTASAPI/src/main/java/com/example/aftasapi/entities/Member.java
+++ API-BACKEND/AFTASAPI/src/main/java/com/example/aftasapi/entities/Member.java
@@ -2,14 +2,19 @@ package com.example.aftasapi.entities;
 
 
 import com.example.aftasapi.entities.enums.IdentityDocumentType;
+import com.example.aftasapi.entities.enums.Role;
 import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import jakarta.persistence.*;
 import lombok.*;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import java.time.LocalDate;
+import java.util.Collection;
 import java.util.List;
 
 @Entity
@@ -18,14 +23,18 @@ import java.util.List;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-public class Member {
+public class Member implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long number;
     private String firstName;
     private String lastName;
+    private String email;
+    private String password;
     private LocalDate accessionDate;
     private String nationality;
+
+    private Role role;
     @Enumerated(EnumType.STRING)
     private IdentityDocumentType identityDocument;
     private String identityNumber;
@@ -33,4 +42,39 @@ public class Member {
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "member")
     @JsonIgnore
     private List<Hunting> huntingList;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
---
+++ API-BACKEND/AFTASAPI/src/main/java/com/example/aftasapi/entities/enums/Role.java
@@ -0,0 +1,5 @@
+package com.example.aftasapi.entities.enums;
+
+public enum Role {
+    ADMIN, USER
+}
