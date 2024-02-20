ec8f8f65159c3e6d2d2ea4ed16f678c0efb0b0ab
ABDELLAHdev01 <abdellahelghoulamdev@gmail.com>
2024-02-20 10:04:50 +0100

    Config security cand JwtService andd make two Dto For login one reponse and other for request

--- API-BACKEND/AFTASAPI/src/main/java/com/example/aftasapi/repositories/MemberRepository.java
+++ API-BACKEND/AFTASAPI/src/main/java/com/example/aftasapi/repositories/MemberRepository.java
@@ -2,8 +2,13 @@ package com.example.aftasapi.repositories;
 
 import com.example.aftasapi.entities.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface MemberRepository  extends JpaRepository<Member,Long> {
+    @Query(value = "SELECT * FROM member u WHERE u.email = :email", nativeQuery = true)
+    Optional<Member> findByEmailNativeQuery( String email);
 }
